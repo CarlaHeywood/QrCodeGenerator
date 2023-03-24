610770350d4c2335de632931bfc706bfbef2dde8
Carla <carlaheywood24@gmail.com>
2023-03-23 18:51:39 -0700

    Make it a great day

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# QrCodeGenerator
+# QR Code Generator
+Create a QR Code for any link displayed on the front end and also creates a downloadable image for reuse. 
+
+```
+    virtualenv env
+    source env/bin/activate
+    pip3 install -r requirements.txt
+    python run.py
+```
---
+++ run.py
@@ -0,0 +1,57 @@
+import os, sys, flask, io, qrcode
+from flask import Flask, request, render_template, redirect, url_for
+import werkzeug
+# from jinja2 import Markup, Environment, FileSystemLoader
+
+from livereload import Server
+
+from qrcode import *
+
+app = Flask(__name__, static_url_path='/qrimg', static_folder='qrimg')
+
+app.config['SECRET_KEY'] = SECRET_KEY = os.urandom(12)
+
+print("*******************************")
+print("*                             *")
+print("*           Welcome!          *")
+print("*                             *")
+print("*******************************")
+
+# Main Page
+@app.route('/', methods=['GET', 'POST'])
+def index():
+        return render_template('index.html', qrcodename="qr_carlaheywood.com.png")
+
+@app.route('/newQRcode', methods=['POST'])
+def makeqrcode():
+    # https://github.com/lincolnloop/python-qrcode
+    
+    weblink = request.form['weblink']
+    print("Weblink: ",weblink)
+
+    # Encoding data using make() function
+    img = qrcode.make(weblink)
+    
+    # Saving as an image file
+    qrcodename = "qr_" + weblink[8:] + ".png"
+    print(qrcodename)
+    img.save('qrimg/' + qrcodename)
+
+    #Creating an instance of qrcode
+    qr = qrcode.QRCode(
+            version=1,
+            box_size=10,
+            border=5)
+    qr.add_data(weblink)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    f = io.StringIO()
+    qr.print_ascii(out=f)
+    f.seek(0)
+    print(f.read())
+    return render_template('index.html', qrcodename=qrcodename)
+
+app.debug = True
+if __name__ == '__main__':
+   server = Server(app.wsgi_app)
+   server.serve()
